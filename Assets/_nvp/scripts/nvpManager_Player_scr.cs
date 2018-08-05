9cbca18ecb3a3bbe0a67acad0d0e17359da62967
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2018-08-05 14:41:30 +0200

    Implemented NavMesh and NavMeshAgent
    Player moves to floor position under mouse click by unsing pathfinding through unity nav mesh.

--- Assets/_nvp/scripts/nvpManager_Player_scr.cs
+++ Assets/_nvp/scripts/nvpManager_Player_scr.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 using newvisionsproject.managers.events;
 
 namespace newvisionsproject.example_unity_0002_nav_mesh 
@@ -8,10 +10,13 @@ namespace newvisionsproject.example_unity_0002_nav_mesh
 	public class nvpManager_Player_scr : MonoBehaviour 
 	{
 		// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+		NavMeshAgent _navAgent;
 
 		// +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		void Start () {
+			
+			_navAgent = this.GetComponent<NavMeshAgent>();
+
 			nvpManager_Events_scr.INSTANCE.SubscribeToEvent(GameEvents.OnFloorPositionSelected, OnFloorPositionSelected);	
 		}
 		
@@ -25,7 +30,7 @@ namespace newvisionsproject.example_unity_0002_nav_mesh
 		// +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		void OnFloorPositionSelected(object s, object e){
 			Vector3 pos = (Vector3)e;
-			this.transform.position = pos; 
+			_navAgent.SetDestination(pos);
 		}
 
 		void OnDestroy(){
