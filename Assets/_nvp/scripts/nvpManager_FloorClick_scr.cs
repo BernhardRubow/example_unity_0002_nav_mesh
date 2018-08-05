3c9b000968f6eaf986e70356fb97b322b440bc16
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2018-08-05 14:14:17 +0200

    Clickable Floor implemented
    Floor is now clickable. Player moves to clicked position immediatly.

---
+++ Assets/_nvp/scripts/nvpManager_FloorClick_scr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using newvisionsproject.managers.events;
+
+namespace newvisionsproject.example_unity_0002_nav_mesh 
+{
+
+	public class nvpManager_FloorClick_scr : MonoBehaviour 
+	{
+		// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		[SerializeField] private Vector3 _clickPosition;
+		[SerializeField] private GameObject _floor;
+		private LayerMask _floorMask;
+
+
+
+		// +++ callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		void Start () {
+			_floorMask = LayerMask.GetMask("nvpFloor");
+		}
+		
+		void Update () {
+			if(Input.GetMouseButtonUp(0)){
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if(Physics.Raycast(ray, out hit, 100, _floorMask))
+				{
+					_clickPosition = hit.point;
+					nvpManager_Events_scr.INSTANCE.InvokeEvent(GameEvents.OnFloorPositionSelected, this, _clickPosition);
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/_nvp/scripts/nvpManager_Player_scr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using newvisionsproject.managers.events;
+
+namespace newvisionsproject.example_unity_0002_nav_mesh 
+{
+	public class nvpManager_Player_scr : MonoBehaviour 
+	{
+		// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+		// +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		void Start () {
+			nvpManager_Events_scr.INSTANCE.SubscribeToEvent(GameEvents.OnFloorPositionSelected, OnFloorPositionSelected);	
+		}
+		
+		void Update () {
+			
+		}
+
+
+
+
+		// +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		void OnFloorPositionSelected(object s, object e){
+			Vector3 pos = (Vector3)e;
+			this.transform.position = pos; 
+		}
+
+		void OnDestroy(){
+			nvpManager_Events_scr.INSTANCE.UnsubscribeFromEvent(GameEvents.OnFloorPositionSelected, OnFloorPositionSelected);
+		}
+	}
+}
